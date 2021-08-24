c08c2f280941768fad7dae10dc9923c00e4a1cd3
qkdlqkdl44 <qkdlqkdl44@gmail.com>
2021-08-25 04:53:42 +0900

    commit

---
+++ programmers/HIGH_SCORE_KIT/BINARY SEARCH/4_징검다리.py
@@ -0,0 +1,31 @@
+#https://programmers.co.kr/learn/courses/30/lessons/43236
+
+def solution(distance, rocks, n):
+    answer = 0
+    
+    left=0
+    right=distance
+    rocks.sort()
+    while left<=right:
+        
+        mid = (left+right)//2
+        #previous stone position
+        pre=0
+        remove=0
+        minD=1000000001
+        for r in rocks:
+            d=r-pre
+            if d<mid:
+                remove+=1
+            else:
+                pre=r
+                minD=min(d,minD)
+
+        #because remove too much stone, mid have to be smaller
+        if remove>n:
+            right=mid-1
+        else:
+            left=mid+1
+            answer=minD
+            
+    return answer
