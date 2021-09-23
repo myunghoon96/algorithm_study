5fa17d499144ffc0577c5bcb8c2889a7f041e9ac
qkdlqkdl44 <qkdlqkdl44@gmail.com>
2021-09-23 16:45:01 +0900

    commit

---
+++ programmers/KAKAO_INTERN_2021/숫자 문자열과 영단어.py
@@ -0,0 +1,12 @@
+#https://programmers.co.kr/learn/courses/30/lessons/81301#
+
+def solution(s):
+    answer = 0
+    ans = ''
+    d = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6',
+        'seven':'7', 'eight':'8', 'nine':'9'}
+    
+    for k,v in d.items():
+        s=s.replace(k,v)
+
+    return int(s)
